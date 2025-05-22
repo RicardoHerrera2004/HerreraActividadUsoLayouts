73254bda5a9102fc55733e0f9870611a592cda2e
laboratorio <laboratorio@UPOAULA10710>
2025-05-22 09:01:45 -0500

    Agregar archivos de proyecto.

---
+++ HerreraActividadUsoLayouts/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HerreraActividadUsoLayouts
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HerreraActividadUsoLayouts/PagGrid.xaml.cs
@@ -0,0 +1,14 @@
+namespace HerreraActividadUsoLayouts;
+
+public partial class PagGrid : ContentPage
+{
+	public PagGrid()
+	{
+		InitializeComponent();
+	}
+
+    private void VolverPaginaPri_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new MainPage());
+    }
+}
